24f44b69c0aab84baac32ee9751a317dadfb8d30
Denis Muniz Silva <82631808+denisms7@users.noreply.github.com>
2025-08-01 13:49:01 -0300

    added Veiculo

--- controle/models.py
+++ controle/models.py
@@ -16,4 +16,22 @@ class Cota(models.Model):
     tipo = models.PositiveSmallIntegerField(default=1, choices=TIPO_CHOICES, verbose_name='Tipo')
 
     def __str__(self):
-        return f'{self.nome} L{self.litros} - {self.littiporos}'
+        return f'{self.nome} L{self.litros} - {self.tipo}'
+
+
+
+class Veiculo(models.Model):
+    COMBUSTIVEL_CHOICES = [
+        (1, _('Gasolina')),
+        (2, _('Diesel S10')),
+        (3, _('Alcool')),
+        ]
+    cadastrado_em = models.DateTimeField(auto_now_add=True, verbose_name=_('Data de Cadastro'))
+    cadastrado_por = models.ForeignKey(User, default=1, on_delete=models.PROTECT, verbose_name=_('Cadastrado por'))
+    descricao = models.CharField(max_length=150, verbose_name=_('Descrição'))
+    placa = models.CharField(max_length=150, verbose_name=_('Placa'))
+    combustivel = models.PositiveSmallIntegerField(default=1, choices=COMBUSTIVEL_CHOICES, verbose_name='Tipo')
+    cota = models.ForeignKey(Cota, verbose_name=_('Cota'), default=1, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.placa} {self.descricao} - {self.combustivel}'
